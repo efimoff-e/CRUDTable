f534822e79176d2301feb1c42fe4016b6a657572
efimoff-e <56920480+efimoff-e@users.noreply.github.com>
2020-06-03 12:12:28 +0300

    Добавьте файлы проекта.

---
+++ CRUDTable/Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace CRUDTable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.dgvInfo = new System.Windows.Forms.DataGridView();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.aboutYourself = new System.Windows.Forms.Label();
+            this.Department = new System.Windows.Forms.Label();
+            this.Adress = new System.Windows.Forms.Label();
+            this.tbAdress = new System.Windows.Forms.TextBox();
+            this.dateOfBirth = new System.Windows.Forms.Label();
+            this.dtBirth = new System.Windows.Forms.DateTimePicker();
+            this.Header = new System.Windows.Forms.Label();
+            this.Patronymic = new System.Windows.Forms.Label();
+            this.tbPatronymic = new System.Windows.Forms.TextBox();
+            this.Surname = new System.Windows.Forms.Label();
+            this.tbSurname = new System.Windows.Forms.TextBox();
+            this.tbDepartment = new System.Windows.Forms.TextBox();
+            this.tbAbout = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(34, 362);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(114, 58);
+            this.btnInsert.TabIndex = 2;
+            this.btnInsert.Text = "Insert";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // dgvInfo
+            // 
+            this.dgvInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInfo.Location = new System.Drawing.Point(12, 452);
+            this.dgvInfo.Name = "dgvInfo";
+            this.dgvInfo.RowHeadersWidth = 51;
+            this.dgvInfo.RowTemplate.Height = 24;
+            this.dgvInfo.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvInfo.Size = new System.Drawing.Size(878, 162);
+            this.dgvInfo.TabIndex = 3;
+            this.dgvInfo.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvInfo_CellClick);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(258, 362);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(114, 58);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(514, 362);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(114, 58);
+            this.btnDelete.TabIndex = 5;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(757, 362);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(114, 58);
+            this.btnReset.TabIndex = 6;
+            this.btnReset.Text = "Clear Fields";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // aboutYourself
+            // 
+            this.aboutYourself.AutoSize = true;
+            this.aboutYourself.Location = new System.Drawing.Point(277, 244);
+            this.aboutYourself.Name = "aboutYourself";
+            this.aboutYourself.Size = new System.Drawing.Size(99, 17);
+            this.aboutYourself.TabIndex = 30;
+            this.aboutYourself.Text = "About yourself";
+            // 
+            // Department
+            // 
+            this.Department.AutoSize = true;
+            this.Department.Location = new System.Drawing.Point(34, 244);
+            this.Department.Name = "Department";
+            this.Department.Size = new System.Drawing.Size(82, 17);
+            this.Department.TabIndex = 28;
+            this.Department.Text = "Department";
+            // 
+            // Adress
+            // 
+            this.Adress.AutoSize = true;
+            this.Adress.Location = new System.Drawing.Point(277, 126);
+            this.Adress.Name = "Adress";
+            this.Adress.Size = new System.Drawing.Size(133, 17);
+            this.Adress.TabIndex = 27;
+            this.Adress.Text = "Residential address";
+            // 
+            // tbAdress
+            // 
+            this.tbAdress.Location = new System.Drawing.Point(277, 145);
+            this.tbAdress.Multiline = true;
+            this.tbAdress.Name = "tbAdress";
+            this.tbAdress.Size = new System.Drawing.Size(613, 59);
+            this.tbAdress.TabIndex = 26;
+            // 
+            // dateOfBirth
+            // 
+            this.dateOfBirth.AutoSize = true;
+            this.dateOfBirth.Location = new System.Drawing.Point(30, 126);
+            this.dateOfBirth.Name = "dateOfBirth";
+            this.dateOfBirth.Size = new System.Drawing.Size(86, 17);
+            this.dateOfBirth.TabIndex = 25;
+            this.dateOfBirth.Text = "Date of birth";
+            // 
+            // dtBirth
+            // 
+            this.dtBirth.Location = new System.Drawing.Point(33, 146);
+            this.dtBirth.Name = "dtBirth";
+            this.dtBirth.Size = new System.Drawing.Size(200, 22);
+            this.dtBirth.TabIndex = 24;
+            // 
+            // Header
+            // 
+            this.Header.AutoSize = true;
+            this.Header.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Header.Location = new System.Drawing.Point(29, 9);
+            this.Header.Name = "Header";
+            this.Header.Size = new System.Drawing.Size(190, 25);
+            this.Header.TabIndex = 23;
+            this.Header.Text = "Employee Card Info:";
+            // 
+            // Patronymic
+            // 
+            this.Patronymic.AutoSize = true;
+            this.Patronymic.Location = new System.Drawing.Point(581, 52);
+            this.Patronymic.Name = "Patronymic";
+            this.Patronymic.Size = new System.Drawing.Size(78, 17);
+            this.Patronymic.TabIndex = 22;
+            this.Patronymic.Text = "Patronymic";
+            // 
+            // tbPatronymic
+            // 
+            this.tbPatronymic.Location = new System.Drawing.Point(584, 71);
+            this.tbPatronymic.Name = "tbPatronymic";
+            this.tbPatronymic.Size = new System.Drawing.Size(306, 22);
+            this.tbPatronymic.TabIndex = 21;
+            // 
+            // Surname
+            // 
+            this.Surname.AutoSize = true;
+            this.Surname.Location = new System.Drawing.Point(275, 52);
+            this.Surname.Name = "Surname";
+            this.Surname.Size = new System.Drawing.Size(65, 17);
+            this.Surname.TabIndex = 20;
+            this.Surname.Text = "Surname";
+            // 
+            // tbSurname
+            // 
+            this.tbSurname.Location = new System.Drawing.Point(278, 71);
+            this.tbSurname.Name = "tbSurname";
+            this.tbSurname.Size = new System.Drawing.Size(255, 22);
+            this.tbSurname.TabIndex = 19;
+            // 
+            // tbDepartment
+            // 
+            this.tbDepartment.Location = new System.Drawing.Point(33, 264);
+            this.tbDepartment.Name = "tbDepartment";
+            this.tbDepartment.Size = new System.Drawing.Size(196, 22);
+            this.tbDepartment.TabIndex = 31;
+            // 
+            // tbAbout
+            // 
+            this.tbAbout.Location = new System.Drawing.Point(278, 264);
+            this.tbAbout.Multiline = true;
+            this.tbAbout.Name = "tbAbout";
+            this.tbAbout.Size = new System.Drawing.Size(612, 59);
+            this.tbAbout.TabIndex = 32;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Name";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(33, 71);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(200, 22);
+            this.tbName.TabIndex = 33;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(902, 626);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbAbout);
+            this.Controls.Add(this.tbDepartment);
+            this.Controls.Add(this.aboutYourself);
+            this.Controls.Add(this.Department);
+            this.Controls.Add(this.Adress);
+            this.Controls.Add(this.tbAdress);
+            this.Controls.Add(this.dateOfBirth);
+            this.Controls.Add(this.dtBirth);
+            this.Controls.Add(this.Header);
+            this.Controls.Add(this.Patronymic);
+            this.Controls.Add(this.tbPatronymic);
+            this.Controls.Add(this.Surname);
+            this.Controls.Add(this.tbSurname);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.dgvInfo);
+            this.Controls.Add(this.btnInsert);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Employee Info App";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInfo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.DataGridView dgvInfo;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label aboutYourself;
+        private System.Windows.Forms.Label Department;
+        private System.Windows.Forms.Label Adress;
+        private System.Windows.Forms.TextBox tbAdress;
+        private System.Windows.Forms.Label dateOfBirth;
+        private System.Windows.Forms.DateTimePicker dtBirth;
+        private System.Windows.Forms.Label Header;
+        private System.Windows.Forms.Label Patronymic;
+        private System.Windows.Forms.TextBox tbPatronymic;
+        private System.Windows.Forms.Label Surname;
+        private System.Windows.Forms.TextBox tbSurname;
+        private System.Windows.Forms.TextBox tbDepartment;
+        private System.Windows.Forms.TextBox tbAbout;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbName;
+    }
+}
+
---
+++ CRUDTable/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace CRUDTable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //инициализируем подключение
+        SqlConnection connection = new SqlConnection(@"Data Source=DESKTOP-V5QB5R7\SQLEXPRESS;Initial Catalog=AppCRUD;Integrated Security=True");
+        //объявляем ID сотрудника
+        public int EmployeeID;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetEmployeeRecord();
+        }
+        private void GetEmployeeRecord()
+        {   // выбираем все записи из БД
+            SqlCommand sqlCommand = new SqlCommand("Select * from EmployeeDb", connection);
+            DataTable dataTable = new DataTable();
+
+            connection.Open();
+
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            dataTable.Load(sqlDataReader);
+            connection.Close();
+
+            dgvInfo.DataSource = dataTable;
+        }
+
+        //вызываем событие по щелчку на Insert
+        private void btnInsert_Click(object sender, EventArgs e)
+        {   /* делаем запрос в БД  EmployeeDb:
+             - вставить в поля таблицы данные из textbox'ов*/
+            if (isValid())
+            {
+                SqlCommand sqlCommand = new SqlCommand("INSERT INTO EmployeeDb VALUES (@employeeName, @employeeSurname, @employeePatronimic, @dateOfBirth, @Adress, @Department, @About)", connection);
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Parameters.AddWithValue("@employeeName", tbName.Text);
+                sqlCommand.Parameters.AddWithValue("@employeeSurname", tbSurname.Text);
+                sqlCommand.Parameters.AddWithValue("@employeePatronimic", tbPatronymic.Text);
+                sqlCommand.Parameters.AddWithValue("@dateOfBirth", dtBirth.Value);
+                sqlCommand.Parameters.AddWithValue("@Adress", tbAdress.Text);
+                sqlCommand.Parameters.AddWithValue("@Department", tbDepartment.Text);
+                sqlCommand.Parameters.AddWithValue("@About", tbAbout.Text);
+                // открываем БД, выполняем запрос, закрываем
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+                connection.Close();
+                //сообщение о том, что операция прошла успешно
+                MessageBox.Show("New Employee has been added!", "Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //живое обновление данными  gridbox'а
+                GetEmployeeRecord();
+                ResetFormControl();
+            }
+        }
+
+        // инициализируем флаг "если true"
+        private bool isValid()
+        { // проверка на заполнение имени сотрудника
+        if(tbName.Text == string.Empty)
+            {
+                MessageBox.Show("Student Name is required", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        //очищаем поля формы
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            ResetFormControl();
+        }
+
+        private void ResetFormControl()
+        {
+            EmployeeID = 0;
+            tbName.Clear();
+            tbSurname.Clear();
+            tbPatronymic.Clear();
+            tbAdress.Clear();
+            tbDepartment.Clear();
+            tbAbout.Clear();
+            
+            tbName.Focus();
+        }
+
+        private void dgvInfo_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //подсветка данных при выборе строки из gridbox'а
+            EmployeeID = Convert.ToInt32(dgvInfo.SelectedRows[0].Cells[0].Value);
+            tbName.Text = dgvInfo.SelectedRows[0].Cells[1].Value.ToString();
+            tbSurname.Text = dgvInfo.SelectedRows[0].Cells[2].Value.ToString();
+            tbPatronymic.Text = dgvInfo.SelectedRows[0].Cells[3].Value.ToString();
+            dtBirth.Text = Convert.ToString(dgvInfo.SelectedRows[0].Cells[4].Value);
+            tbAdress.Text = dgvInfo.SelectedRows[0].Cells[5].Value.ToString();
+            tbDepartment.Text = dgvInfo.SelectedRows[0].Cells[6].Value.ToString();
+            tbAbout.Text = dgvInfo.SelectedRows[0].Cells[7].Value.ToString();
+        }
+        // обновляем данные сотрудника
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if(EmployeeID > 0)
+            {
+                SqlCommand sqlCommand = new SqlCommand("UPDATE EmployeeDb SET employeeName = @employeeName, employeeSurname = @employeeSurname, employeePatronimic = @employeePatronimic, dateOfBirth = @dateOfBirth, Adress = @Adress, Department = @Department, About = @About WHERE EmployeeID = @id", connection);
+                sqlCommand.CommandType = CommandType.Text;
+                sqlCommand.Parameters.AddWithValue("@employeeName", tbName.Text);
+                sqlCommand.Parameters.AddWithValue("@employeeSurname", tbSurname.Text);
+                sqlCommand.Parameters.AddWithValue("@employeePatronimic", tbPatronymic.Text);
+                sqlCommand.Parameters.AddWithValue("@dateOfBirth", Convert.ToString(dtBirth.Value));
+                sqlCommand.Parameters.AddWithValue("@Adress", tbAdress.Text);
+                sqlCommand.Parameters.AddWithValue("@Department", tbDepartment.Text);
+                sqlCommand.Parameters.AddWithValue("@About", tbAbout.Text);
+                sqlCommand.Parameters.AddWithValue("@id", this.EmployeeID);
+
+                // открываем БД, выполняем запрос, закрываем
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+                connection.Close();
+
+                MessageBox.Show("Employee info has been updated", "Updated", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //живое обновление данными  gridbox'а
+                GetEmployeeRecord();
+
+                ResetFormControl();
+            }
+            else
+            {
+                MessageBox.Show("Select employee to update", "Select?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+        // удаляем запись из БД
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if(EmployeeID > 0)
+            {
+                SqlCommand sqlCommand = new SqlCommand("DELETE FROM EmployeeDb WHERE EmployeeID = @id", connection);
+                sqlCommand.CommandType = CommandType.Text;
+
+                sqlCommand.Parameters.AddWithValue("@id", this.EmployeeID);
+
+                // открываем БД, выполняем запрос, закрываем
+                connection.Open();
+                sqlCommand.ExecuteNonQuery();
+                connection.Close();
+
+                MessageBox.Show("Employee info has been deleted", "Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                //живое обновление данными  gridbox'а
+                GetEmployeeRecord();
+
+                ResetFormControl();
+            }
+            else
+            {
+                MessageBox.Show("Select employee to delete his information", "Select?", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+        }
+    }
+}
